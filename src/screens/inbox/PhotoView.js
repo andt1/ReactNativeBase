403687798b4765d675784de5018d472a97876558
binhtx <binhtx@runsystem.net>
2020-02-24 14:23:01 +0700

    - function save image to storage

--- src/screens/inbox/PhotoView.js
+++ src/screens/inbox/PhotoView.js
@@ -1,19 +1,44 @@
 import React from 'react';
-import {View,Image} from 'react-native';
+import {View, Image, TouchableOpacity, Text, Alert} from 'react-native';
+import CameraRoll from '@react-native-community/cameraroll';
 
-function PhotoView ({navigation }) {
+function PhotoView({navigation}) {
 
-        const data = navigation.getParam('dataImage');
+    const data = navigation.getParam('dataImage');
 
-        return(
-            <View>
-                <Image
-                    source={{uri:data.uri}}
-                    style={{ width: 350, height: 350 }}
-                />
-            </View>
-        )
+    return (
+        <View>
+            <Image
+                source={{uri: data.uri}}
+                style={{width: 350, height: 350}}
+            />
+
+            <TouchableOpacity style={{
+                height: 50,
+                alignSelf: 'center',
+                marginTop: 20,
+                backgroundColor: '#81d4fa',
+                justifyContent: 'center',
+                borderRadius: 10,
+                padding: 10,
+            }}
+                              onPress={() => {
+                                  CameraRoll.saveToCameraRoll(data.uri)
+                                      .then(r => {
+                                          console.log('successful uri', r);
+                                          Alert.alert('', 'Save Image to storage: ' + r);
+                                      })
+                                      .catch((err) => {
+                                          console.warn('error', err);
+                                          Alert.alert('', 'Error: ' + err);
+                                      });
+
+                              }}>
+                <Text>Save Image</Text>
+            </TouchableOpacity>
+        </View>
+    );
 }
 
-export default PhotoView
+export default PhotoView;
 
